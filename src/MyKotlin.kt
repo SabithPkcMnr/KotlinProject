69831d99999d9faf799d9c8bc5054bca5eeff96a
Sabith Pkc Mnr <sabithpkcmnr@gmail.com>
2018-04-23 15:49:40 +0530

    YouTube Kotlin Project

---
+++ src/MyKotlin.kt
@@ -0,0 +1,11 @@
+fun main(superArg: Array<String>) {
+
+    println("Enter your first name: ")
+    var first_name = readLine()
+    println("Enter your second name: ")
+    var second_name = readLine()
+    print("\n\nThanks for the info\n\n")
+    var full_name = "Your fullname is "+first_name + " " + second_name
+    print(full_name+"\n")
+
+}
